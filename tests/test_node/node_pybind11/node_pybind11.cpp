df608d5c8c504d5d2fc3ba5fb8c55afd1305eaa3
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2023-08-04 20:50:01 +0200

    update test

--- tests/test_node/node_pybind11/node_pybind11.cpp
+++ tests/test_node/node_pybind11/node_pybind11.cpp
@@ -4,11 +4,12 @@
 namespace py = pybind11;
 using namespace py::literals;
 
-PYBIND11_MODULE(node_pybind11, m) {
+PYBIND11_MODULE(node_pybind, m) {
     m.doc() = "pybind11 node example";
 
     py::class_<Node, Node_SP>(m, "Node")
-        .def(py::init(&Node::allocate),  "allocate width, height", "w"_a, "h"_a)
+        // .def(py::init(&Node::allocate),  "allocate width, height", "w"_a, "h"_a)
+        .def_static("allocate", &Node::allocate,  "allocate width, height", "w"_a, "h"_a)
         .def_property("w", &Node::get_width, &Node::set_width)
         .def_property("h", &Node::get_height, &Node::set_height)
         ;
