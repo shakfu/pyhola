f0f0d453830db28d72327fa1172c8d09d982dc70
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2023-08-04 20:51:58 +0200

    update test

--- tests/test_node/node_pybind11/node_pybind11.cpp
+++ tests/test_node/node_pybind11/node_pybind11.cpp
@@ -8,7 +8,6 @@ PYBIND11_MODULE(node_pybind, m) {
     m.doc() = "pybind11 node example";
 
     py::class_<Node, Node_SP>(m, "Node")
-        // .def(py::init(&Node::allocate),  "allocate width, height", "w"_a, "h"_a)
         .def_static("allocate", &Node::allocate,  "allocate width, height", "w"_a, "h"_a)
         .def_property("w", &Node::get_width, &Node::set_width)
         .def_property("h", &Node::get_height, &Node::set_height)
